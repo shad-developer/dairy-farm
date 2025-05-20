dcd562db30ce9a0a20e4149c904167374763e794
Muhammad Umair Shad <151988665+shad-developer@users.noreply.github.com>
2025-05-20 12:38:28 +0500

    index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://dairy-farm-ry3z.onrender.com",
     credentials: true,
   })
 );
